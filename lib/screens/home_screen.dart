e5d88f95b26ca41c0f22e0e50ea764b612319bbf
JITHU MATHEWS <jithumathews111@gmail.com>
2023-02-24 11:00:32 +0530

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key, required this.email});
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome $email'),
+      ),
+    );
+  }
+}
